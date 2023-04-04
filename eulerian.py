e54d5d0a848b0460cb62ba573c6ae3d76db4f48a
David Brewster <brewcrew5498@outlook.com>
2023-04-04 15:25:29 -0400

    stg

--- eulerian.py
+++ eulerian.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
+import functools
+import heapq
 import itertools
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-import heapq
 
 from collections import defaultdict
-from decimal import Decimal
+from matplotlib.colors import LinearSegmentedColormap
 from typing import DefaultDict, Dict, Generic, List, Tuple, TypeVar
 
 def eulerian(N):
   G = nx.DiGraph()
   G.add_nodes_from(range(N))
-  yield from _eulerian(N, 0, 0, G, 0, N)
+  for d in range(N+1):
+    yield from _eulerian(N, 0, 0, G, 0, d)
 
 
 def _is_eulerian(G: nx.DiGraph) -> bool:
@@ -30,11 +32,13 @@ def _eulerian(N, i, j, G: nx.DiGraph, acc_deg, last_deg):
     return 
 
   if i == N and j == 0:
-    if _is_eulerian(G) and nx.is_weakly_connected(G):
+    if _is_eulerian(G) and nx.is_weakly_connected(G): # and nx.is_regular(G):
       yield G.copy()
     return
 
   if j == N:
+    # # Regularity check.
+    # if last_deg == acc_deg:
     yield from _eulerian(N, i+1, 0, G, 0, acc_deg)
     return
 
@@ -124,33 +128,102 @@ class MaxExamples(Generic[Value, Example]):
   def get(self) -> None:
     return self.heap[::-1]
       
+@functools.lru_cache(maxsize=None)
+def make_binary(test_config, N):
+  idxs = set(test_config)
+  return tuple(int(idx in idxs) for idx in range(N))
+
+
+@functools.lru_cache(maxsize=None)
+def get_col(config) -> int:
+  """TODO: optimize this"""
+  N = len(config)
+  r = sum(config)
+  for idx, test_config in enumerate(itertools.combinations(range(N), r=r)):
+    if make_binary(test_config, N) == config:
+      return idx
+  raise ValueError("no idx found")
+  
+@functools.lru_cache(maxsize=None)
+def get_row(config) -> int:
+  return sum(config)
+
+
+def draw_stg(G: nx.DiGraph) -> None:
+  """Draw the state transition graph"""
+  N = len(G)
+  STG = nx.MultiDiGraph()
+  V = list(itertools.product((0, 1), repeat=N))
+  STG.add_nodes_from(V)
+  for u in G.nodes():
+    for (_, v) in G.out_edges(u):
+      for config in V:
+        next_config = config[:v] + (config[u],) + config[v+1:]
+        STG.add_edge(config, next_config)
+
+  pos: Dict[Tuple[int], Tuple[int, int]] = {}
+  for config in V:
+    pos[config] = (get_row(config), get_col(config))
+
+  nx.draw_networkx_nodes(STG, pos)
+  edge_colors = []
+  edge_weights = []
+  edgelist = []
+  seen = set()
+  cmap = LinearSegmentedColormap.from_list('rg', ["r", "y", "g"]) 
+  # cmap=plt.cm.Blues
+  print(cmap)
+  for u, v in STG.edges():
+    left, right = sorted((u, v), key=get_row)
+    if (left, right) in seen: continue
+    outgoing = STG.number_of_edges(left, right)
+    incoming = STG.number_of_edges(right, left)
+    weight = incoming + outgoing
+    if weight == 0: continue
+    seen.add((left, right))
+    edgelist.append((left, right))
+    edge_weights.append(weight)
+    corr = (outgoing - incoming) / weight
+    edge_colors.append(corr)
+
+  print(edge_colors)
+  nx.draw_networkx_edges(STG, pos=pos, edgelist=edgelist, edge_color=edge_colors, width=edge_weights, edge_cmap=cmap, edge_vmin=-1, edge_vmax=+1, arrows=False)
+
+
+def draw(G: nx.DiGraph, prefix: str) -> None:
+  print(f"{prefix} {r=}, {time=}, {G.edges()=}")
+  fig = plt.figure()
+  fig.set_figheight(7)
+  fig.set_figwidth(7)
+  plt.subplot(211)  
+  plt.title(f"{prefix} {N=}, {r=}, {time=:.2f} steps")
+  nx.draw(G)
+  plt.subplot(212)
+  draw_stg(G)
+  plt.show()
+
 
 if __name__ == '__main__':
-  N = 5
+  N = 4
   count = 0
   K = 1
+  SHOW = True
   # r -> (time, G)
   max_exp_abs_time_by_r: DefaultDict[float, MaxExamples[float, nx.DiGraph]] = defaultdict(lambda: MaxExamples(K))
   by_degree_seq = defaultdict(list)
   for G in eulerian(N):
     if (d := deg_seq(G)) not in by_degree_seq or not any(nx.is_isomorphic(G, H) for H in by_degree_seq[d]):
       by_degree_seq[d].append(G)
-      for r in (0.1,):
+      for r in (100,):
         time = expected_absorption_time(G, r)
-        if max_exp_abs_time_by_r[r].add(time, G):
-          print(f"Update! {r=}, {time=}, {G.edges()=}")
-          plt.figure()  
-          plt.title(f"{N=}, {r=}, {time=:.2f} steps")
-          nx.draw(G)
-          plt.show()
+        if max_exp_abs_time_by_r[r].add(time, G) and SHOW:
+          draw(G, "Update!")
 
       count += 1
   
   print(f"{count=}")
-  for r, examples in max_exp_abs_time_by_r.items():
-    for (time, G) in examples.get():
-      print(f"Winner! {r=}, {time=}, {G.edges()=}")
-      plt.figure()  
-      plt.title(f"Winner: {N=}, {r=}, {time=:.2f} steps")
-      nx.draw(G)
-      plt.show()
+
+  if SHOW:
+    for r, examples in max_exp_abs_time_by_r.items():
+      for (time, G) in examples.get():
+        draw(G, "Winner!")
